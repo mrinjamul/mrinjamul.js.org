cc559ef47ed85d3eadd61cc71c07a75673a9585c
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2019-08-14 22:34:15 +0530

    update AboutMe

--- src/component/AboutMe.js
+++ src/component/AboutMe.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 function AboutMe() {
     return(
-        <h1>About Me</h1>
+        <div className="section">
+        <h1><span>About Me</span></h1>
+        <p>
+            Hey! I'm <strong>Alex</strong>. Coding has changed my world. It's not just about apps. Learning to code gave me <i>problem-solving skills</i> and a way to communicate with others on a technical level. I can also develop websites and use my coding skills to get a better job. And I learned it all at <strong>SoloLearn</strong> where they build your self-esteem and keep you motivated. Join me in this rewarding journey. You'll have fun, get help, and learn along the way!
+        </p>
+        <p className="quote">"Declare variables, not war"</p>
+        </div>
+
     );
 }
 
