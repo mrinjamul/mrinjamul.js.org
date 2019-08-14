cc29f41e2723d44c2b2abc88f80a65d43cc8e5e2
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2019-08-14 22:05:53 +0530

    update Header

--- src/component/Header.js
+++ src/component/Header.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 function Header() {
     return(
-        <h1>Header</h1>
+        <div id="header" className="section">
+            <img alt="" className="img-circle" src="https://code.sololearn.com/Icons/Avatars/0.jpg"/>
+            <p>Injamul Mohammad Mollah</p>
+        </div>
     );
 }
 
