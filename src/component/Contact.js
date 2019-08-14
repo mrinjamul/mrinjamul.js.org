acf6467594c8e90ee3dcfb337270de3d7f3ad286
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2019-08-14 22:34:29 +0530

    update  Contact

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -2,7 +2,21 @@ import React from 'react';
 
 function Contact() {
     return(
-        <h1>Contact</h1>
+        <div className="section" id="contacts">
+        <h1><span>Follow Me</span></h1>
+        <div>
+            <a href="https://www.sololearn.com/" >
+                <img alt="SoloLearn" src="https://www.sololearn.com/Uploads/icons/sololearn.png" />
+            </a>
+            <a href="https://www.sololearn.com/">
+                <img alt="Facebook" src="https://www.sololearn.com/Uploads/icons/facebook.png"/>
+            </a>
+            <a href="https://www.sololearn.com/">
+                <img alt="Twitter" src="https://www.sololearn.com/Uploads/icons/twitter.png" />
+            </a>
+        </div>
+    </div>
+
     );
 }
 
