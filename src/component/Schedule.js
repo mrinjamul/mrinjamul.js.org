5b56b90d4e355eab48691d275c5851f4dcee18f6
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2019-08-14 22:35:12 +0530

    update Schedule

--- src/component/Schedule.js
+++ src/component/Schedule.js
@@ -2,7 +2,60 @@ import React from 'react';
 
 function Schedule() {
     return(
-        <h1>Schedule</h1>
+        <div className="section">
+        <h1><span>My Coding Schedule</span></h1>
+        <table>
+            <tr>
+                <th>Day</th>
+                <th>Mon</th>
+                <th>Tue</th>
+                <th>Wed</th>
+                <th>Thu</th>
+                <th>Fri</th>
+            </tr>
+            <tr>
+                <td>8-8:30</td>
+                <td className="selected">Learn</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>9-10</td>
+                <td></td>
+                <td className="selected">Practice</td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>1-1:30</td>
+                <td></td>
+                <td></td>
+                <td className="selected">Play</td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>3:45-5</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td className="selected">Code</td>
+                <td></td>
+            </tr>
+            <tr>
+                <td>6-6:15</td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td className="selected">Discuss</td>
+            </tr>
+        </table>
+    </div>
+
     );
 }
 
