79aefe32a860971aa20eb5cbb07cd6addf4914bc
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2019-08-15 09:59:31 +0530

    add Display Picture

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import Avatar from './Uploads/icons/0.jpg';
+
 function Header() {
     return(
         <div id="header" className="section">
-            <img alt="" className="img-circle" src="https://code.sololearn.com/Icons/Avatars/0.jpg"/>
+            <img alt="" className="img-circle" src={Avatar} />
             <p>Injamul Mohammad Mollah</p>
         </div>
     );
