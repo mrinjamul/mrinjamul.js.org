11b34ea5c6d70fea1b2cb019f4501978e08507d7
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2019-08-15 18:05:51 +0530

    update Message

--- src/component/Message.js
+++ src/component/Message.js
@@ -3,9 +3,9 @@ import React from 'react';
 function Message(props) {
     return(
         <div>
-            <h3 >Name: {this.state.name}</h3>
-            <h3>Email: {this.state.email}</h3>
-            <h3>Message: {this.state.message}</h3>
+            <h3 >Name: {props.data.name}</h3>
+            <h3>Email: {props.data.email}</h3>
+            <h3>Message: {props.data.message}</h3>
             <hr/>
 
         </div>
