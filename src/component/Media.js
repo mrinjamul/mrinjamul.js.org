c1fa880060b6e3857f657dc0a92b099688dc545d
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2019-08-15 10:15:01 +0530

    update Media

--- src/component/Media.js
+++ src/component/Media.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import logo from '../logo.svg';
 
 function Media() {
     return(
         <div className="section">
         <h1><span>My Media</span></h1>
-
+        <img src={logo} className="App-logo" alt="logo" />
     </div>
     );
 }
