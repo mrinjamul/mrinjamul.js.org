b25dbf8dc9b37410855b85000be6144d6c66e1bb
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2019-08-15 10:14:50 +0530

    remove logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Blog from './component/Blog'
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
 
         <Blog/>
       </header>
