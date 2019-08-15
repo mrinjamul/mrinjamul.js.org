04308fab7e31e8d9d45083bffda17d5620ee4b59
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2019-08-15 21:16:55 +0530

    update CSS

--- src/component/Message.js
+++ src/component/Message.js
@@ -4,12 +4,13 @@ function Message(props) {
     return(
         <div className="section">
             <hr/>
+            <div className="index">
+                <h3>Index no.: {props.index}</h3>
+            </div>
             <h3 >Name: {props.name}</h3>
             <h3>Email: {props.email}</h3>
             <h3>Message: {props.message}</h3>
-            <div className="copyright">
-                <h3>Index no.: {props.index}</h3>
-            </div>
+            <br />
             <hr/>
 
         </div>
