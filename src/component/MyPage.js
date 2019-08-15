a52fa560b3519b55b0233fec94740ed80ffa0a9c
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2019-08-15 14:00:32 +0530

    update MyPage

--- src/component/MyPage.js
+++ src/component/MyPage.js
@@ -4,7 +4,27 @@ function MyPage() {
     return(
         <div className="section">
         <h1><span>My Pages</span></h1>
+        <ul>
+            <li>
+                <a href="https://mrinjamul.github.io" >Home</a>
+            </li>
+            <li>
+                <a href="https://mrinjamul.github.io/myschedule" >My College Schedule</a>
+            </li>
+            <li>
+                <a href="https://mrinjamul.github.io/calculator-html" >Calculator by HTML</a>
+            </li>
+            <li>
+                <a href="https://mrinjamul.github.io/snake-2d" >Snake 2D</a>
+            </li>
+            <li>
+                <a href="https://mrinjamul.github.io/Chess-2D" >Chess 2D</a>
+            </li>
 
+            <li>
+                <a href="https://mrinjamul.github.io/myapp" >My App [In Development]</a>
+            </li>
+        </ul>
     </div>
     );
 }
