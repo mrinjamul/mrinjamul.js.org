ac6e66b70fa860724ff9855a36a46b99f9267fe0
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2019-08-15 10:52:02 +0530

    add Submit function

--- src/component/Form.js
+++ src/component/Form.js
@@ -4,9 +4,39 @@ class Form extends Component {
     constructor() {
         super()
         this.state = {
-
+            "name": "",
+            "email": "",
+            "message": ""
         }
     }
+
+    handleChange = (event) => {
+        const {name, value, type, checked} = event.target
+        type === "checkbox" ? 
+            this.setState({
+                [name]: checked
+            })
+        :
+        this.setState({
+            [name]: value
+        }) 
+      }
+      
+    handleSubmit=(event)=>{
+        event.preventDefault();
+        console.log(this.state);
+        fetch('http://localhost:8080/viewers/',{
+            method: 'POST',
+            headers: {'Content-type':'application/json'},
+            body: JSON.stringify({
+                "name": this.state.name,
+                "email": this.state.email,
+                "message": this.state.message
+            })
+        });
+        alert("Message sent");
+    }
+
     render() {
         return(
             <div className="section">
@@ -21,10 +51,10 @@ class Form extends Component {
                 <path d="M 30 70 q 20 20 40 0" stroke="#FFFFFF" stroke-width="5" fill="none" />
             </svg>
                  
-            <form>
-                <input name="name" placeholder="Name" type="text" required /><br/>
-                <input name="email" placeholder="Email" type="email" required /><br/>
-                <textarea name="message" placeholder="Message" required ></textarea>
+            <form onSubmit={this.handleSubmit} >
+                <input name="name" value={this.state.name} placeholder="Name" type="text" required onChange={this.handleChange} /><br/>
+                <input name="email" value={this.state.email} placeholder="Email" type="email" required onChange={this.handleChange} /><br/>
+                <textarea name="message" value={this.state.message} placeholder="Message" required onChange={this.handleChange} ></textarea>
                 <input type="submit" value="SEND" className="submit" />
             </form>
         </div>
