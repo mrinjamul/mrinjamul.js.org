aedeb4fdf02a3caf4843e2af3e64b6b4e80e5169
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2019-08-15 20:51:48 +0530

    Reader configured with sever

--- src/component/Reader.js
+++ src/component/Reader.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Message from './Message';
-import messageData from './messageData';
 
-function Reader(props) {
-    const messageCompo = messageData.map(m => <Message key={m.id} name={m.name} email={m.email} message={m.message} />);
-    return(
-        <div>
-            {messageCompo}
-        </div>
-    );
+class Reader extends Component {
+    constructor() {
+        super()
+        this.state = {
+            name: "",
+            email: "",
+            message: "",
+            visitors: []
+        }
+    }
+    componentDidMount(){
+        fetch("http://localhost:8080/viewers/")
+            .catch(err=>console.log(err))
+            .then(res=>res.json())
+            .then(res=>{
+                this.setState({
+                    visitors: res
+                });
+                console.log(res);
+            });
+        
+}
+
+
+    render() {
+        var visitors = this.state.visitors;
+		var myvisitors=visitors.map((m,index) => (<Message key={m.id} name={m.name} email={m.email} message={m.message} />));
+
+        return(
+            <div>
+                {myvisitors}
+            </div>
+        );
+    }
 }
 
 export default Reader;
