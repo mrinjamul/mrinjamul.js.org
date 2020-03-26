4ca331286e7a05ce987b7438e60b5b5389c6d4f2
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2020-03-26 22:36:08 +0530

    updated Motto

--- src/component/Motto.js
+++ src/component/Motto.js
@@ -5,7 +5,7 @@ function Motto() {
     return(
         <div className="section">
         <h1><span>My Motto</span></h1>
-
+        <p><qoute>"If you want to fly give up everythings that weights you down."</qoute></p>
     </div>
     );
 }
