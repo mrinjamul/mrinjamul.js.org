ae03db0fbf81474f8ca2fdbb9e39b1e1cc40b988
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2020-07-07 23:21:15 +0530

    Add: My website

---
+++ precache-manifest.197bbbbdf90f4e78de9569185cc94445.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c7f62f2d54c93ec394f570596904f55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6eeebebbfdb71a7e4da",
+    "url": "/static/css/main.dcc07989.chunk.css"
+  },
+  {
+    "revision": "af4fc2b653de975851d4",
+    "url": "/static/js/2.19085ffa.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/static/js/2.19085ffa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6eeebebbfdb71a7e4da",
+    "url": "/static/js/main.5aa85a18.chunk.js"
+  },
+  {
+    "revision": "9e45163c8b544142db9f",
+    "url": "/static/js/runtime-main.3a4e94a2.js"
+  },
+  {
+    "revision": "081fff63df8b087a25261c8019c403c8",
+    "url": "/static/media/0.081fff63.jpg"
+  },
+  {
+    "revision": "0904b5505a2c9f39585d67b603844739",
+    "url": "/static/media/header.0904b550.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3296fa6e1dc60baf2769df6f8b4e9100",
+    "url": "/static/media/sololearn.3296fa6e.png"
+  }
+]);
