82f49e5d61bd98457111f4629b0bd87d546b6b11
Injamul Mohammad Mollah <mrinjamul@gmail.com>
2020-08-16 19:48:07 +0530

    fix: minor changes

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyArAtF4XBrbA4eq1vuhVZUo1LdtGrtyOS0",
-  authDomain: "injamul-7465c.firebaseapp.com",
-  databaseURL: "https://injamul-7465c.firebaseio.com",
-  projectId: "injamul-7465c",
-  storageBucket: "injamul-7465c.appspot.com",
-  messagingSenderId: "714873804548",
-  appId: "1:714873804548:web:ae8565fd81a31c6faae251",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
